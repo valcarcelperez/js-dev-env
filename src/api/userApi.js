0a7076bb69780facd8afd116979becb5a29c0fea
valcarcelperez <ernestov@mtolympus.partners.directs.com>
2017-05-12 17:06:02 -0500

    HTTP Calls. Part 2. Generate mock data. Added Api server for returning generated data. (json-server)

--- src/api/userApi.js
+++ src/api/userApi.js
@@ -1,17 +1,20 @@
 import 'whatwg-fetch'; // this polyfill ensures that fetch runs in browsers that do not have fetch support.
+import getBaseUrl from './baseUrl';
 
-export function getUsers(){
-    return get('users');
+const baseUrl = getBaseUrl();
+
+export function getUsers() {
+  return get('users');
 }
 
-function get(url){
-    return fetch(url).then(onSuccess, onError);
+function get(url) {
+  return fetch(baseUrl + url).then(onSuccess, onError);
 }
 
-function onSuccess(response){
-    return response.json();
+function onSuccess(response) {
+  return response.json();
 }
 
-function onError(error){
-    console.log(error); // eslint-disable-line no-console
+function onError(error) {
+  console.log(error); // eslint-disable-line no-console
 }
