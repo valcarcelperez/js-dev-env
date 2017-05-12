4423bc119466706a05ef65baa0f7d9f9df92b4f4
valcarcelperez <valcarcel_perez@hotmail.com>
2017-05-12 08:05:43 -0500

    HTTP Calls. Part 1.

--- buildScripts/srcServer.js
+++ buildScripts/srcServer.js
@@ -19,6 +19,14 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, '../src/index.html'))
 });
 
+app.get('/users', function (req, res) {
+  // Hard coding for simplicity.
+  res.json([
+    { "id": 1, "firstName": "firstName1", "lastName": "lastName1", "email": "email1@server.com" },
+    { "id": 2, "firstName": "firstName2", "lastName": "lastName2", "email": "email2@server.com" },
+    { "id": 3, "firstName": "firstName3", "lastName": "lastName3", "email": "email3@server.com" }
+  ]);
+});
 
 app.listen(port, function (err) {
   if (err) {
