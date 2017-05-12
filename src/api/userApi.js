bfbcdd6165ae404024c1a9b086c0be532587e088
valcarcelperez <ernestov@mtolympus.partners.directs.com>
2017-05-12 10:01:50 -0500

    HTTP Calls. Part 2. Added user Api using fetch.

---
+++ src/api/userApi.js
@@ -0,0 +1,17 @@
+import 'whatwg-fetch'; // this polyfill ensures that fetch runs in browsers that do not have fetch support.
+
+export function getUsers(){
+    return get('users');
+}
+
+function get(url){
+    return fetch(url).then(onSuccess, onError);
+}
+
+function onSuccess(response){
+    return response.json();
+}
+
+function onError(error){
+    console.log(error); // eslint-disable-line no-console
+}
