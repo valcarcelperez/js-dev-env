98f2558847d0fcee2fe395af6e908cf27fb26c11
Ernesto Valcarcel <valcarcel_perez@hotmail.com>
2017-05-13 22:03:09 -0500

    HTTP Calls. Part 2. Implemented user delete.

--- src/index.js
+++ src/index.js
@@ -1,19 +1,34 @@
-import { getUsers } from './api/userApi';
+import { getUsers, deleteUser } from './api/userApi';
 
 // Populate table of users via API call
 getUsers().then(result => {
-    let usersBody = "";
+  let usersBody = "";
 
-    result.forEach(user => {
-        usersBody += `
+  result.forEach(user => {
+    usersBody += `
             <tr>
-                <td><a href="#" data-id="${user.id}" class=deleteUser">Delete</a></td>
+                <td><a href="#" data-id="${user.id}" class="deleteUser">Delete</a></td>
                 <td>${user.id}<td>
                 <td>${user.firstName}<td>
                 <td>${user.lastName}<td>
                 <td>${user.email}<td>
             </tr>`
-    });
+  });
 
-    global.document.getElementById('users').innerHTML = usersBody;
+  global.document.getElementById('users').innerHTML = usersBody;
+
+  const deleteLinks = global.document.getElementsByClassName('deleteUser');
+  // debugger; // eslint-disable-line no-debugger
+
+  // Must user array.from to create a real array from a DOM collection
+  // getElementByClassName only returns an "array link" object
+  Array.from(deleteLinks, link => {
+    link.onclick = function (event) {
+      const element = event.target;
+      event.preventDefault();
+      deleteUser(element.attributes["data-id"].value);
+      const row = element.parentNode.parentNode;
+      row.parentNode.removeChild(row);
+    };
+  });
 });
